5fca897f0c7b424147c6656eb14d31acce66eaf0
Tyler Becks <tylerdbecks@gmail.com>
2020-10-19 09:03:54 -0700

    Add Log Rocket

--- apollo-config.ts
+++ apollo-config.ts
@@ -1,6 +1,6 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://devoted-salmon-28.hasura.app/v1/graphql',
+  uri: 'https://devoted-salmon-28.hasura.app/v1/graphql', // TODO
   cache: new InMemoryCache(),
 });
